7f7ec910608a8cc66e4b7f4f7acc8a94cbdb098a
LeAlbus <jpedro_lopes21@hotmail.com>
2015-04-16 14:52:48 -0300

    Start

---
+++ Homepwner/Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by Pedro Lopes on 4/16/15.
+//  Copyright (c) 2015 Pedro Lopes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
