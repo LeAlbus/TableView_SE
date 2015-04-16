246089fb03e612c6ecc8b5fe2c8b68ab901e7a7f
LeAlbus <jpedro_lopes21@hotmail.com>
2015-04-16 17:12:30 -0300

    Adicionada Classe Items

---
+++ Homepwner/Homepwner/SEItem.h
@@ -0,0 +1,39 @@
+//
+//  SEItem.h
+//  Homepwner
+//
+//  Created by Pedro Lopes on 4/16/15.
+//  Copyright (c) 2015 Pedro Lopes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SEItem : NSObject
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
++ (instancetype)randomItem;
+
+// initializer do SEItem
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+
+@end
